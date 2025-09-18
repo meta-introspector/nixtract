62fb2d6d2033debe50b387d2f84a1c6b241b5df3
mike <h@solfunmeme.com>
2025-09-18 23:36:55 +0000

    working

--- flake.nix
+++ flake.nix
@@ -4,12 +4,19 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
+    rusty-hook-src.url = "path:../../../../vendor/hooks/rusty-hook"; # Reference parent's rusty-hook
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+  outputs = { self, nixpkgs, flake-utils, rusty-hook-src, ... }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        rusty-hook-pkg = pkgs.rustPlatform.buildRustPackage {
+          pname = "rusty-hook";
+          version = "0.11.2"; # Match the version used in parent's .pre-commit-config.yaml
+          src = rusty-hook-src;
+          cargoLock = rusty-hook-src + "/Cargo.lock";
+        };
       in
       {
         devShells.default = pkgs.mkShell {
@@ -20,6 +27,7 @@
             pre-commit # Add pre-commit to the devShell
             shellcheck # Add shellcheck to the devShell
             direnv # Add direnv to the devShell
+            rusty-hook-pkg # Add rusty-hook to the devShell
           ];
 
           shellHook = ''
